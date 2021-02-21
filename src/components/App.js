e27b751a5233f1f6c15a3b50803de34a5677b945
allexib <ooomegasib@gmail.com>
2021-02-21 18:56:03 +0400

    9\16 238 The Connect Function

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import SongLost from './SongList';
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <SongLost/>
+        </div>
+    );
 };
 
 export default App;
